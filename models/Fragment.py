042737d611d71d681d46199389fd3eb469638019
Herhar Fares <herhar.fares@yandex.com>
2020-08-14 21:12:21 +0100

    /models --> DOC 1.0

--- models/Fragment.py
+++ models/Fragment.py
@@ -1,21 +1,76 @@
 class Fragment:
-    def __init__(self):
-        self.sequence = ""
-        self.lenght = -1
-        self.index = -1
-        self.orientation = True
+    """This is a Fragment class that represent a Fragment for the
+    dna sequencing problem.
+    I could noave not used this class, but for the records, and keeping
+    track of every data, and to make things make more sense, I defined it.
 
-    def __init__(self, sequence, lenght, index, orientation=True):
+    ...
+
+    Attributes
+    ----------
+    sequence: str
+        This is the DNA sequence of the fragment.
+    lenght: int
+        The lenght of the read DNA sequence.
+    index: int
+        When reading the file, to extract fragments I keep the index
+        of each fragments in the file. Not necessary, neither
+        for the execution, nor the presentation,
+        but for debugging(the solution's validity) needs. 
+    orientation: bool
+        Durring the run time, we need to know the orientation of the fragment
+        from left to right if True, False else.
+
+    Methods
+    -------
+    __init__(orientation=True): None
+        The constructor.
+    __str__: str
+        The print formating method.
+    inverse:
+        For inversing the DNA sequence string.
+
+    """
+
+    def __init__(self, sequence, lenght, index):
+        """The constructor
+
+        ...
+
+        Parameters
+        ----------
+        sequence: str
+            This is the DNA sequence of the fragment.
+        lenght: int
+            The lenght of the read DNA sequence.
+        index: int
+            When reading the file, to extract fragments I keep the index
+            of each fragments in the file. Not necessary, neither
+            for the execution, nor the presentation,
+            but for debugging(the solution's validity) needs. 
+
+
+        Rturns
+        ------
+        None
+        """
+
         self.sequence = sequence
         self.lenght = lenght
         self.index = index
         self.orientation = True
 
     def inverse(self):
+        """The inverse method inverses the DNA sequence string."""
+        
         self.sequence = self.sequence[::-1]
         self.orientation = not self.orientation
 
     def __str__(self):
+        """This method returns the formating print format, to print out
+        a fragment, while all the details all printed.
+        """
+
         out = "* Sequence:: {}\n* The Lenght:: {}\n* The Index:: {}\n"
         if self.orientation:
             out += "* From Left to Right."
--- models/Solution.py
+++ models/Solution.py
@@ -1,16 +1,70 @@
 class Solution:
-    def __init__(self):
-        self.genome = list()
-        self.genome_size = -1
-        self.oaf = -1
-        self.odf = -1
+    """This is a Solution class that represent a Solution for the
+    dna sequencing problem.
 
-    def __init__(self, genome, oaf=-1, odf=-1):
+    ...
+
+    Attributes
+    ----------
+    genome: list
+        The list of the Fragments that represents a solution.
+    genome_size: int
+        The lenght of the genome(the solution array).
+    oaf: int
+        The value of the first objective function, overlaping adjacent
+        fragments.
+    odf: int
+        The value of the second objective function, overlaping distinct
+        fragments.
+    generation: int
+        The number(index) of the generation, that can tell at what
+        generation the solution was created.
+
+    Methods
+    -------
+    __init__(oaf=-1, odf=-1, generation=-1): None
+        The constructor.
+    __str__: str
+        The print formating method.
+    """
+
+    def __init__(self, genome, oaf=-1, odf=-1, generation=-1):
+        """The constructor
+
+        ...
+
+        Parameters
+        ----------
+        genome: list
+            The list of the Fragments that represents a solution.
+        oaf: int, optional
+            The value of the first objective function, overlaping adjacent
+            fragments. Durring the run time this variable will be
+            assigned by the first objective function.
+        odf: int, optional
+            The value of the second objective function, overlaping distinct
+            fragments.  Durring the run time this variable will be
+            assigned by the second objective function.
+        genration: int, optional
+            The number(index) of the generation, that can tell at what
+            generation the solution was created. Durring the run time,
+            once the Solution is computed, the generation will be assigned.
+
+        Rturns
+        ------
+        None
+        """
+
         self.genome = genome
         self.genome_size = len(self.genome)
         self.oaf = oaf
         self.odf = odf
+        self.generation = generation
 
     def __str__(self):
-        out = "* Genome:: {}\n* Genome size:: {}\n* OAF::{}\n* ODF:: {}"
-        return out.format(self.genome, self.genome_size, self.oaf, self.odf)
+        """This method returns the formating print format, to print out
+        a solution, while all the details all printed.
+        """
+
+        out = "* Genome:: {}\n* Genome size:: {}\n* OAF::{}\n* ODF:: {}\n* Generation:: {}"
+        return out.format(self.genome, self.genome_size, self.oaf, self.odf, self.generation)
