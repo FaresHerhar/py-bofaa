6dcb75f6053a1c4187a9ece923b66c1bd9cdbdab
Herhar Fares <herhar.fares@yandex.com>
2020-08-24 19:10:57 +0100

    update config

--- config.py
+++ config.py
@@ -1,10 +1,23 @@
+benchmarks = [
+    "benchmarks/test.dat",
+    "benchmarks/dna-instances/x60189_4.dat",
+    "benchmarks/dna-instances/x60189_5.dat",
+    "benchmarks/dna-instances/x60189_6.dat",
+    "benchmarks/dna-instances/x60189_7.dat",
+    "benchmarks/dna-instances/m15421_5.dat",
+    "benchmarks/dna-instances/m15421_6.dat",
+    "benchmarks/dna-instances/m15421_7.dat",
+    "benchmarks/dna-instances/38524243_4.dat",
+    "benchmarks/dna-instances/38524243_7.dat",
+    "benchmarks/dna-instances/j02459_7.dat"
+]
+
 CROSS_OVER_PROBABILITY = 0.8
 MUTATION_PROBABILITY = 0.8
-GENERATIONS_NUMBER = 100
-POPULATION_SIZE = 10
+GENERATIONS_NUMBER = 2000
+POPULATION_SIZE = 400
 OVECTIVE_FUNCTIONS_NUMBER = 2
 MATCH_SCORE = 1
 MISMATCH_SCORE = -1
 GAP_COST = -1.33
-BECHMARK_FILE = "benchmarks/dna-instances/x60189_4.dat"
-# BECHMARK_FILE = "benchmarks/test.dat"
+BECHMARK_FILE = benchmarks[1]
